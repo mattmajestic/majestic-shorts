de5ba25b0931a297e210dba275dfeca35e2fbbe1
Matt Majestic <33904170+mattmajestic@users.noreply.github.com>
2024-07-13 16:02:55 -0400

    first commit

---
+++ src/config.ts
@@ -0,0 +1,4 @@
+export const WebsiteName: string = "Majestic Shorts"
+export const WebsiteBaseUrl: string = "https://majesticshorts.io"
+export const WebsiteDescription: string =
+  "Open source, fast, and free to host SaaS template. Built with SvelteKit, Supabase, Stripe, Tailwind, DaisyUI, and Postgres"
---
+++ src/routes/(marketing)/+page.svelte
@@ -0,0 +1,304 @@
+<script lang="ts">
+  import {
+    WebsiteName,
+    WebsiteBaseUrl,
+    WebsiteDescription,
+  } from "./../../config"
+
+  const ldJson = {
+    "@context": "https://schema.org",
+    "@type": "WebSite",
+    name: WebsiteName,
+    url: WebsiteBaseUrl,
+  }
+  const jsonldScript = `<script type="application/ld+json">${
+    JSON.stringify(ldJson) + "<"
+  }/script>`
+
+  const features = [
+    {
+      name: "Video Generator",
+      description:
+        "Create YouTube Shorts quickly. Free to start, and cost effective to scale.",
+      link: "https://github.com/CriticalMoments/CMSaasStarter/blob/main/README.md#pricing",
+      linkText: "Video Editor",
+      newPage: true,
+      svgContent: `<path d="M4.01207 15.7618L5.70156 10.6933C6.46758 8.39525 6.85059 7.24623 7.75684 7.03229C8.6631 6.81835 9.51953 7.67478 11.2324 9.38764L14.6114 12.7666C16.3242 14.4795 17.1807 15.3359 16.9667 16.2422C16.7528 17.1484 15.6038 17.5314 13.3057 18.2975L8.23724 19.987C5.47183 20.9088 4.08912 21.3697 3.35924 20.6398C2.62936 19.9099 3.09026 18.5272 4.01207 15.7618Z" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round"/>
+<path d="M12.2351 18.3461C12.2351 18.3461 11.477 16.0649 11.477 14.5552C11.477 13.0454 12.2351 10.7643 12.2351 10.7643M8.06517 19.4833C8.06517 19.4833 7.42484 16.7314 7.307 14.9343C7.11229 11.965 8.06517 7.35254 8.06517 7.35254" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round"/>
+<path d="M14.5093 10.0061L14.6533 9.28614C14.7986 8.55924 15.3224 7.96597 16.0256 7.73155C16.7289 7.49714 17.2526 6.90387 17.398 6.17697L17.542 5.45703" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round"/>
+<path d="M17.5688 12.2533L17.7817 12.3762C18.4388 12.7556 19.265 12.6719 19.8327 12.1685C20.3468 11.7126 21.0789 11.597 21.7085 11.8723L22 11.9997" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round"/>
+<path d="M10.5352 3C10.1977 3.55206 10.2823 4.26344 10.7399 4.72097L10.8377 4.81885C11.2309 5.21201 11.3759 5.78959 11.215 6.32182" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round"/>
+<path d="M13.561 4.39648C13.7621 4.19542 13.8626 4.09489 13.9788 4.05804C14.0772 4.02688 14.1827 4.02688 14.281 4.05804C14.3973 4.09489 14.4978 4.19542 14.6989 4.39648C14.8999 4.59753 15.0004 4.69806 15.0373 4.8143C15.0685 4.91262 15.0685 5.01817 15.0373 5.11648C15.0004 5.23272 14.8999 5.33325 14.6989 5.53431C14.4978 5.73536 14.3973 5.83589 14.281 5.87274C14.1827 5.90391 14.0772 5.90391 13.9788 5.87274C13.8626 5.83589 13.7621 5.73536 13.561 5.53431C13.36 5.33325 13.2594 5.23272 13.2226 5.11648C13.1914 5.01817 13.1914 4.91262 13.2226 4.8143C13.2594 4.69806 13.36 4.59753 13.561 4.39648Z" fill="#1C274C"/>
+<path d="M19.4682 7.46822C19.7136 7.22283 19.8363 7.10014 19.9747 7.04681C20.1367 6.9844 20.3161 6.9844 20.4781 7.04681C20.6165 7.10014 20.7392 7.22283 20.9846 7.46822C21.23 7.71362 21.3527 7.83631 21.406 7.97472C21.4684 8.1367 21.4684 8.31609 21.406 8.47807C21.3527 8.61649 21.23 8.73918 20.9846 8.98457C20.7392 9.22996 20.6165 9.35266 20.4781 9.40599C20.3161 9.4684 20.1367 9.4684 19.9747 9.40599C19.8363 9.35266 19.7136 9.22996 19.4682 8.98457C19.2228 8.73918 19.1001 8.61649 19.0468 8.47807C18.9844 8.31609 18.9844 8.1367 19.0468 7.97472C19.1001 7.83631 19.2228 7.71362 19.4682 7.46822Z" fill="#1C274C"/>
+<path d="M6.92712 3.94079C7.13659 3.73132 7.47621 3.73132 7.68567 3.94079C7.89514 4.15026 7.89514 4.48988 7.68567 4.69935C7.47621 4.90882 7.13659 4.90882 6.92712 4.69935C6.71765 4.48988 6.71765 4.15026 6.92712 3.94079Z" fill="#1C274C"/>
+<path d="M19.058 15.3134C19.2674 15.1039 19.6071 15.1039 19.8165 15.3134C20.026 15.5228 20.026 15.8624 19.8165 16.0719C19.6071 16.2814 19.2674 16.2814 19.058 16.0719C18.8485 15.8624 18.8485 15.5228 19.058 15.3134Z" fill="#1C274C"/>
+<path d="M17.5 9.74145C17.7095 9.53198 18.0491 9.53198 18.2586 9.74145C18.468 9.95092 18.468 10.2905 18.2586 10.5C18.0491 10.7095 17.7095 10.7095 17.5 10.5C17.2905 10.2905 17.2905 9.95092 17.5 9.74145Z" fill="#1C274C"/>`,
+    },
+    {
+      name: "User Auth",
+      link: "/login",
+      linkText: "Login",
+      description:
+        "Sign up, sign out, forgot password, email verification, and oAuth. Powered by Supabase Auth.",
+      svgContent: `<path
+                  d="M14.2084 13.5521C16.3025 13.5521 18 11.8615 18 9.77606C18 7.6906 16.3025 6 14.2084 6C12.1144 6 10.4169 7.6906 10.4169 9.77606C10.4169 10.742 10.8578 11.4446 10.8578 11.4446L6.27264 16.011C6.0669 16.2159 5.77886 16.7486 6.27264 17.2404L6.8017 17.7673C7.00743 17.9429 7.52471 18.1888 7.94796 17.7673L8.56519 17.1526C9.18242 17.7673 9.88782 17.416 10.1523 17.0647C10.5932 16.45 10.0642 15.8353 10.0642 15.8353L10.2405 15.6597C11.087 16.5027 11.8277 16.011 12.0922 15.6597C12.5331 15.045 12.0922 14.4303 12.0922 14.4303C11.9159 14.079 11.5632 14.079 12.004 13.64L12.5331 13.113C12.9564 13.4643 13.8264 13.5521 14.2084 13.5521Z"
+                  stroke="#1C274C"
+                  stroke-width="1.5"
+                  stroke-linejoin="round"
+                />
+                <path
+                  d="M22 12C22 16.714 22 19.0711 20.5355 20.5355C19.0711 22 16.714 22 12 22C7.28595 22 4.92893 22 3.46447 20.5355C2 19.0711 2 16.714 2 12C2 7.28595 2 4.92893 3.46447 3.46447C4.92893 2 7.28595 2 12 2C16.714 2 19.0711 2 20.5355 3.46447C21.5093 4.43821 21.8356 5.80655 21.9449 8"
+                  stroke="#1C274C"
+                  stroke-width="1.5"
+                  stroke-linecap="round"
+                />`,
+    },
+    {
+      name: "Pricing",
+      link: "/pricing",
+      linkText: "Checkout Pricing",
+      description:
+        "Sign Up for the Pro Tier to create more content.",
+      svgContent: `<path d="M12 6V18" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round"/>
+<path d="M15 9.5C15 8.11929 13.6569 7 12 7C10.3431 7 9 8.11929 9 9.5C9 10.8807 10.3431 12 12 12C13.6569 12 15 13.1193 15 14.5C15 15.8807 13.6569 17 12 17C10.3431 17 9 15.8807 9 14.5" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round"/>
+<path d="M7 3.33782C8.47087 2.48697 10.1786 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 10.1786 2.48697 8.47087 3.33782 7" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round"/>`,
+    },
+    {
+      name: "Subscriptions",
+      link: "/pricing",
+      linkText: "Upgrade to Pro",
+      description: "Upgrade your Account via Stripe Checkout.",
+      svgContent: `<path d="M2 12C2 8.22876 2 6.34315 3.17157 5.17157C4.34315 4 6.22876 4 10 4H14C17.7712 4 19.6569 4 20.8284 5.17157C22 6.34315 22 8.22876 22 12C22 15.7712 22 17.6569 20.8284 18.8284C19.6569 20 17.7712 20 14 20H10C6.22876 20 4.34315 20 3.17157 18.8284C2 17.6569 2 15.7712 2 12Z" stroke="#1C274C" stroke-width="1.5"/>
+<path d="M10 16.5H6" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round"/>
+<path d="M8 13.5H6" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round"/>
+<path d="M2 10L22 10" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round"/>
+<path d="M14 15C14 14.0572 14 13.5858 14.2929 13.2929C14.5858 13 15.0572 13 16 13C16.9428 13 17.4142 13 17.7071 13.2929C18 13.5858 18 14.0572 18 15C18 15.9428 18 16.4142 17.7071 16.7071C17.4142 17 16.9428 17 16 17C15.0572 17 14.5858 17 14.2929 16.7071C14 16.4142 14 15.9428 14 15Z" stroke="#1C274C" stroke-width="1.5"/>`,
+    },
+    {
+      name: "User Dashboard",
+      link: "/account/settings",
+      linkText: "View Settings",
+      description:
+        "User profile, user settings, update email/password, billing, and more.",
+      svgContent: `<circle cx="12" cy="12" r="3" stroke="#1C274C" stroke-width="1.5"/>
+<path d="M13.7654 2.15224C13.3978 2 12.9319 2 12 2C11.0681 2 10.6022 2 10.2346 2.15224C9.74457 2.35523 9.35522 2.74458 9.15223 3.23463C9.05957 3.45834 9.0233 3.7185 9.00911 4.09799C8.98826 4.65568 8.70226 5.17189 8.21894 5.45093C7.73564 5.72996 7.14559 5.71954 6.65219 5.45876C6.31645 5.2813 6.07301 5.18262 5.83294 5.15102C5.30704 5.08178 4.77518 5.22429 4.35436 5.5472C4.03874 5.78938 3.80577 6.1929 3.33983 6.99993C2.87389 7.80697 2.64092 8.21048 2.58899 8.60491C2.51976 9.1308 2.66227 9.66266 2.98518 10.0835C3.13256 10.2756 3.3397 10.437 3.66119 10.639C4.1338 10.936 4.43789 11.4419 4.43786 12C4.43783 12.5581 4.13375 13.0639 3.66118 13.3608C3.33965 13.5629 3.13248 13.7244 2.98508 13.9165C2.66217 14.3373 2.51966 14.8691 2.5889 15.395C2.64082 15.7894 2.87379 16.193 3.33973 17C3.80568 17.807 4.03865 18.2106 4.35426 18.4527C4.77508 18.7756 5.30694 18.9181 5.83284 18.8489C6.07289 18.8173 6.31632 18.7186 6.65204 18.5412C7.14547 18.2804 7.73556 18.27 8.2189 18.549C8.70224 18.8281 8.98826 19.3443 9.00911 19.9021C9.02331 20.2815 9.05957 20.5417 9.15223 20.7654C9.35522 21.2554 9.74457 21.6448 10.2346 21.8478C10.6022 22 11.0681 22 12 22C12.9319 22 13.3978 22 13.7654 21.8478C14.2554 21.6448 14.6448 21.2554 14.8477 20.7654C14.9404 20.5417 14.9767 20.2815 14.9909 19.902C15.0117 19.3443 15.2977 18.8281 15.781 18.549C16.2643 18.2699 16.8544 18.2804 17.3479 18.5412C17.6836 18.7186 17.927 18.8172 18.167 18.8488C18.6929 18.9181 19.2248 18.7756 19.6456 18.4527C19.9612 18.2105 20.1942 17.807 20.6601 16.9999C21.1261 16.1929 21.3591 15.7894 21.411 15.395C21.4802 14.8691 21.3377 14.3372 21.0148 13.9164C20.8674 13.7243 20.6602 13.5628 20.3387 13.3608C19.8662 13.0639 19.5621 12.558 19.5621 11.9999C19.5621 11.4418 19.8662 10.9361 20.3387 10.6392C20.6603 10.4371 20.8675 10.2757 21.0149 10.0835C21.3378 9.66273 21.4803 9.13087 21.4111 8.60497C21.3592 8.21055 21.1262 7.80703 20.6602 7C20.1943 6.19297 19.9613 5.78945 19.6457 5.54727C19.2249 5.22436 18.693 5.08185 18.1671 5.15109C17.9271 5.18269 17.6837 5.28136 17.3479 5.4588C16.8545 5.71959 16.2644 5.73002 15.7811 5.45096C15.2977 5.17191 15.0117 4.65566 14.9909 4.09794C14.9767 3.71848 14.9404 3.45833 14.8477 3.23463C14.6448 2.74458 14.2554 2.35523 13.7654 2.15224Z" stroke="#1C274C" stroke-width="1.5"/>`,
+    },
+    {
+      name: "Contact Us",
+      link: "/contact_us",
+      linkText: "Send Message",
+      description:
+        "Contact form for customers to reach out for demos, quotes, and questions.",
+      svgContent: `<path d="M10.5 22V20M14.5 22V20" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round"/>
+<path d="M11 20V20.75H11.75V20H11ZM14 19.25C13.5858 19.25 13.25 19.5858 13.25 20C13.25 20.4142 13.5858 20.75 14 20.75V19.25ZM17.5 5.25C17.0858 5.25 16.75 5.58579 16.75 6C16.75 6.41421 17.0858 6.75 17.5 6.75V5.25ZM7 5.25C6.58579 5.25 6.25 5.58579 6.25 6C6.25 6.41421 6.58579 6.75 7 6.75V5.25ZM9 19.25C8.58579 19.25 8.25 19.5858 8.25 20C8.25 20.4142 8.58579 20.75 9 20.75V19.25ZM15 20.75C15.4142 20.75 15.75 20.4142 15.75 20C15.75 19.5858 15.4142 19.25 15 19.25V20.75ZM10.25 11.25V20H11.75V11.25H10.25ZM11 19.25H4.23256V20.75H11V19.25ZM2.75 17.3953V11.25H1.25V17.3953H2.75ZM4.23256 19.25C3.51806 19.25 2.75 18.5323 2.75 17.3953H1.25C1.25 19.1354 2.48104 20.75 4.23256 20.75V19.25ZM6.5 6.75C8.46677 6.75 10.25 8.65209 10.25 11.25H11.75C11.75 8.04892 9.50379 5.25 6.5 5.25V6.75ZM6.5 5.25C3.49621 5.25 1.25 8.04892 1.25 11.25H2.75C2.75 8.65209 4.53323 6.75 6.5 6.75V5.25ZM21.25 11.25V17.4253H22.75V11.25H21.25ZM19.7931 19.25H14V20.75H19.7931V19.25ZM21.25 17.4253C21.25 18.5457 20.4934 19.25 19.7931 19.25V20.75C21.5305 20.75 22.75 19.1488 22.75 17.4253H21.25ZM22.75 11.25C22.75 8.04892 20.5038 5.25 17.5 5.25V6.75C19.4668 6.75 21.25 8.65209 21.25 11.25H22.75ZM7 6.75H18V5.25H7V6.75ZM9 20.75H15V19.25H9V20.75Z" fill="#1C274C"/>
+<path d="M5 16H8" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round"/>
+<path d="M16 9.88432V5.41121M16 5.41121V2.63519C16 2.39905 16.1676 2.19612 16.3994 2.15144L16.8855 2.05779C17.4738 1.94443 18.0821 1.99855 18.6412 2.214L18.7203 2.24451C19.2746 2.4581 19.8807 2.498 20.4582 2.35891C20.7343 2.2924 21 2.50168 21 2.78573V5.00723C21 5.2442 20.8376 5.45031 20.6073 5.5058L20.5407 5.52184C19.9095 5.67387 19.247 5.63026 18.6412 5.39679C18.0821 5.18135 17.4738 5.12722 16.8855 5.24058L16 5.41121Z" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round"/>`,
+    },
+  ]
+</script>
+
+<svelte:head>
+  <title>{WebsiteName}</title>
+  <meta name="description" content={WebsiteDescription} />
+  <!-- eslint-disable-next-line svelte/no-at-html-tags -->
+  {@html jsonldScript}
+</svelte:head>
+
+<div class="hero min-h-[60vh]">
+  <div class="hero-content text-center py-12">
+    <div class="max-w-xl">
+      <div
+        class="text-xl md:text-3xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-primary to-accent mb-4 md:mb-8"
+      >
+        Majestic Shorts
+      </div>
+
+      <div class="text-4xl md:text-6xl font-bold px-2" style="line-height: 1.2;">
+        The
+        <span class="underline decoration-secondary decoration-4 md:decoration-[6px]">
+          fast
+        </span> and
+        <span class="underline decoration-secondary decoration-4 md:decoration-[6px]">
+          easy way
+        </span>
+        to create
+        <span class="underline decoration-secondary decoration-4 md:decoration-[6px]">
+          YouTube Shorts
+        </span>
+      </div>
+      <div class="mt-6 md:mt-10 text-sm md:text-lg">
+        Built with <a
+          href="https://kit.svelte.dev"
+          class="link font-bold"
+          target="_blank">SvelteKit</a
+        >,
+        <a href="https://supabase.com" class="link font-bold" target="_blank"
+          >Supabase</a
+        >,
+        <a href="https://stripe.com" class="link font-bold" target="_blank"
+          >Stripe</a
+        >,
+        <a href="https://tailwindcss.com" class="link font-bold" target="_blank"
+          >Tailwind</a
+        >,
+        <a href="https://daisyui.com" class="link font-bold" target="_blank"
+          >DaisyUI</a
+        >,
+        <a
+          href="https://www.neon.tech"
+          class="link font-bold"
+          target="_blank">Neon Postgres</a
+        >,
+        <a
+          href="https://hub.docker.com/"
+          class="link font-bold"
+          target="_blank">Docker</a
+        >, and 
+        <a
+          href="https://cloud.google.com/"
+          class="link font-bold"
+          target="_blank">Google Cloud</a
+        >
+      </div>
+      <div class="mt-6 md:mt-2">
+        <a href="https://github.com/CriticalMoments/CMSaasStarter">
+          <button class="btn btn-primary btn-sm px-6">★ us on Github</button>
+        </a>
+        <a
+          href="https://github.com/mattmajestic/saas-starter"
+        >
+          <button class="btn btn-outline btn-primary btn-sm px-6 mt-3 mx-2"
+            >Read the Docs</button
+          >
+        </a>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="min-h-[60vh]">
+  <div class="pt-20 pb-8 px-7">
+    <div class="max-w-lg mx-auto text-center">
+      <div
+        class="text-3xl md:text-5xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-primary to-accent"
+      >
+        Features
+      </div>
+      <div class="mt-6 text-xl font-bold">
+        Checkout offerings
+        <span
+          class="underline decoration-secondary decoration-[3px] md:decoration-[4px]"
+        >
+          of Majestic Shorts
+        </span>
+      </div>
+    </div>
+
+    <div
+      class="flex gap-6 mt-12 max-w-[1064px] mx-auto place-content-center flex-wrap"
+    >
+      {#each features as feature}
+        <div class="card bg-white w-[270px] min-h-[300px] flex-none shadow-xl">
+          <div class="card-body items-center text-center p-[24px] pt-[32px]">
+            <div>
+              <svg
+                width="50px"
+                height="50px"
+                class="mb-2 mt-1"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <!-- eslint-disable-next-line svelte/no-at-html-tags -->
+                {@html feature.svgContent}
+              </svg>
+            </div>
+            <h2 class="card-title">
+              {feature.name}
+            </h2>
+            <p class="text-sm">
+              {feature.description}
+            </p>
+            {#if feature.link}
+              <a
+                href={feature.link}
+                class="pb-4"
+                target={feature.newPage ? "_blank" : ""}
+              >
+                <button
+                  class="btn btn-xs btn-outline rounded-full btn-primary min-w-[100px]"
+                  >{feature.linkText ? feature.linkText : "Try It"}</button
+                >
+              </a>
+            {/if}
+          </div>
+        </div>
+      {/each}
+    </div>
+  </div>
+</div>
+<div class="hero min-h-[60vh] mt-12">
+  <div class="hero-content text-center pb-16 pt-4 px-4">
+    <div class="max-w-lg">
+      <div
+        class="text-3xl md:text-5xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-primary to-accent mt-4"
+      >
+        See it in Action
+      </div>
+      <div
+        class="flex flex-col lg:flex-row mt-8 gap-6 place-content-center content-center"
+      >
+        <div class="hidden md:block">
+          <a href="https://criticalmoments.io" target="_blank" class="link">
+            <div class="mockup-browser border">
+              <div class="mockup-browser-toolbar">
+                <div class="input" style="background:#eee;">
+                  https://criticalmoments.io
+                </div>
+              </div>
+              <div class="flex justify-center">
+                <img
+                  alt="Screenshot of criticalmoments.io homepage"
+                  class="aspect-[2044/1242]"
+                  src="/images/example-home.png"
+                />
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="md:hidden">
+          <a href="https://criticalmoments.io" target="_blank" class="link">
+            <div class="card shadow-xl border overflow-hidden">
+              <img
+                alt="Screenshot of criticalmoments.io homepage"
+                class="aspect-[2044/1242]"
+                src="/images/example-home.png"
+              />
+            </div></a
+          >
+        </div>
+        <div class="min-w-[270px] lg:min-w-[420px] flex mt-6 lg:mt-0">
+          <div class="my-auto">
+            <div class="px-4 text-lg md:text-xl">
+              <a href="https://criticalmoments.io" class="" target="_blank"
+                >Majestic Shorts was created by <span
+                  class="font-bold whitespace-nowrap">Majestic Coding</span
+                >: YouTube Channel
+                <span class="underline decoration-secondary decoration-[3px]"
+                  >as a Full-Stack SaaS.</span
+                ></a
+              >
+            </div>
+            
+            <div class="mt-4 text-large">
+              <a href="/" target="_blank">
+                <button class="btn btn-primary btn-wide mt-3"
+                  >Video Editor</button
+                >
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
